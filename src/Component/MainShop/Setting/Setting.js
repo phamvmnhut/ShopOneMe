2a5efaf7514c2197a134225c4c8fe97fc04338b6
phamvmnhut <phamvmnhut@gmail.com>
2020-04-02 20:44:22 +0700

    update package.json and rename app android

--- src/Component/MainShop/Setting/Setting.js
+++ src/Component/MainShop/Setting/Setting.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import {Button} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class Setting extends Component {
   constructor(props) {
@@ -12,6 +14,27 @@ export default class Setting extends Component {
       <View>
         <Text style={{fontSize: 30, color: '#63ed07'}}> Setting Component</Text>
         <Text>Cảm ơn bạn đã chuyển qua đến tận tab này :))</Text>
+        <Button title="Solid Button" />
+        <Button title="Outline button" type="outline" />
+        <Button title="Clear button" type="clear" />
+        <Button
+          icon={<Icon name="arrow-right" size={15} color="white" />}
+          title="Button with icon component"
+        />
+        <Button
+          icon={{
+            name: 'arrow-right',
+            size: 15,
+            color: 'white',
+          }}
+          title="Button with icon object"
+        />
+        <Button
+          icon={<Icon name="arrow-right" size={15} color="white" />}
+          iconRight
+          title="Button with right icon"
+        />
+        <Button title="Loading button" loading />
       </View>
     );
   }
